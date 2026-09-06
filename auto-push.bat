db9ca4e2dccfb60251ab47660b530c564560f5e4
Gustav Eichelbrenner <gustaveichelbrenner@gmail.com>
2026-09-06 10:38:17 +0300

    ╨н╨║╨╖╨░╨╝╨╡╨╜╨░╤Ж╨╕╨╛╨╜╨╜╤Л╨╡ ╨▓╨╛╨┐╤А╨╛╤Б╤Л: ╨╛╨▒╨╜╨╛╨▓╨╗╨╡╨╜╨╛

--- auto-push.bat
+++ auto-push.bat
@@ -1,7 +1,6 @@
 @echo off
 
-copy /Y "C:\Users\howard\Desktop\Obsidian folders\ВУЗ\5 сем\MLDA\Экзаменационные вопросы\Экзаменационные вопросы.md" "C:\Users\howard\Desktop\mlda\mlda\Экзаменационные вопросы.md"
-cd /d "C:\Users\howard\Desktop\mlda\mlda"
+copy /Y "C:\Users\howard\Desktop\Obsidian folders\ВУЗ\5 сем\MLDA\Экзаменационные вопросы\Экзаменационные вопросы.md" "%~dp0"
 git add .
 git commit -m "Экзаменационные вопросы: обновлено"
 git push origin main
