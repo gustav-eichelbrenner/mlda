ba22b94d2a2408769763bed83a73cff7d5554632
Gustav Eichelbrenner <gustaveichelbrenner@gmail.com>
2026-09-06 10:43:51 +0300

    Экзаменационные вопросы: обновлено

--- auto-push.bat
+++ auto-push.bat
@@ -1,6 +1,8 @@
 @echo off
+chcp 65001 > nul
 
 copy /Y "C:\Users\howard\Desktop\Obsidian folders\ВУЗ\5 сем\MLDA\Экзаменационные вопросы\Экзаменационные вопросы.md" "%~dp0"
+
 git add .
 git commit -m "Экзаменационные вопросы: обновлено"
 git push origin main
